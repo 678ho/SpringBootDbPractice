5bd174558f1c1ddc831790a8a5d0e9eb2096362f
kwangho kwak <678hoa@gmail.com>
2021-07-19 15:27:14 +0900

    add Paging test code

--- src/test/java/org/zerock/ez2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ez2/repository/MemoRepositoryTests.java
@@ -4,8 +4,11 @@ package org.zerock.ez2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.zerock.ez2.entity.Memo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -53,5 +56,19 @@ public class MemoRepositoryTests {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Total Pages : "+result.getTotalPages()); // 페이지 수
+        System.out.println("Total Count : "+result.getTotalElements()); // 전체 갯수
+        System.out.println("Page Number : "+result.getNumber()); // 전체 페이지 번호 ( 0부터 시작 )
+        System.out.println("Page Size : "+result.getSize()); // 페이지당 데이터 갯수
+        System.out.println("has next page? : "+result.hasNext()); // 다음 페이지 존재 여부 확인
+        System.out.println("first page? : "+result.isFirst()); // 첫페이지인지 여부 확인
     }
 }
